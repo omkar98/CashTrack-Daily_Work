1fb735c0376ba970243f7516dd6c9aeecf17f6c7
omkar98 <edu.omkar@gmail.com>
2018-06-29 18:11:27 +0530

    Done with the required text of Breadcrumb
    Question: For a brand like Berroca
    Brand : Berocca | More Energy & Stress related products | Caps : 30s
    
    Do we provide links seperately to Energy and Stress or a comman link to both of them?

--- wp-content/themes/flatsome/woocommerce/global/breadcrumb.php
+++ wp-content/themes/flatsome/woocommerce/global/breadcrumb.php
@@ -6,8 +6,6 @@ div#breadcrumb {
 }
 
 </style>
-
-
 <?php
 /**
  * Shop breadcrumb
@@ -16,95 +14,39 @@ div#breadcrumb {
  * @package 	WooCommerce/Templates
  * @version     2.3.0
  * @see         woocommerce_breadcrumb()
+ 
+ A backup of this file is stored in breadcrumb-backup.php
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-  
 
-
-if ( !empty($breadcrumb) ) {
-
-  
-  //  do_action('flatsome_before_breadcrumb');
-//
-	//echo $wrap_before;
-//    
-//    //print_r($breadcrumb);
-//
-//
-//	foreach ( $breadcrumb as $key => $crumb ) {
-//       
-//                
-//		echo $before;
-//		if ( ! empty( $crumb[1] ) && sizeof( $breadcrumb ) !== $key + 1 ) {
-//			echo '<a href="' . esc_url( $crumb[1] ) . '">' . esc_html( $crumb[0] ) . '</a>';
-//           
-//        
-//		} else if(!is_product() && !flatsome_option('wc_category_page_title')) {
-//			echo esc_html( $crumb[0] );
-//            
-//		}
-//        if($key == 2){
-//            echo '<span class = "divider" style = "font-weight:300;"> &nbsp;| Caps :&nbsp;'. substr($crumb[0],-3,3).'</span>';
-//          
-//        }
-//
-//		echo $after;
-//
-//		// Single product or Active title
-//		if(is_product() || flatsome_option('wc_category_page_title')){
-//				$key = $key+1;
-//				if ( sizeof( $breadcrumb ) > $key+1) {
-//					//echo ' <span class="divider">'.$delimiter.'</span> ';
-//                    echo '<span class="divider">:</span>';
-//				}
-//		} else{
-//			
-//		// Category pages
-//		if ( sizeof( $breadcrumb ) !== $key + 1 ) {
-//				//echo ' <span class="divider">'.$delimiter.'</span> ';
-//                                echo '<span class="divider">:</span>';
-//
-//			}
-//		}
-//
-//	}
-//
-       
+if ( !empty($breadcrumb) ) {    
 	echo $wrap_after;
     //Fetch Needs And Brand
     $product_id = get_the_ID();
-//echo get_the_title($product_id);
 ?>
 <div id = "breadcrumb">
 <?php
+    
+    /*This gets all the terms of a particular product ID*/
 $terms = get_the_terms ( $product_id, 'product_cat' );
-print_r($terms);
+//print_r($terms);
     $count = 0;
-    $brand;
-foreach ( $terms as $term ) 
-{
-    if($term->$term_id == 94)
-    {
-        $brand = $term->name;
-    }
-    if($term->$term_id == 15)
-    {
-        $needs = $term->name;
-    }
-}
-$terms = get_the_terms ( $product_id, 'product_cat' );
+/*This loop is for Brand*/  
 foreach ( $terms as $term ) 
 {   
     if($term->parent == 94)
     {
-     echo "$brand : $term->name";
-     echo '<a href="'.home_url( ).'/product-category/'.$brand.'/'.$term->name.'">' . '</a>';
+//     echo  "Brand : " . $term->name;
+     echo '<a href="'.home_url( ).'/product-category/brand/">Brand : </a>';
+     echo '<a href="'.home_url( ).'/product-category/brand/'.strtolower($term->name).'">' .$term->name. '</a>';
     }
 }   
+    
+/*This loop is for Type*/
  $terms = get_the_terms ( $product_id, 'product_cat' );
  $count = 0;
  foreach ( $terms as $term ) 
@@ -113,15 +55,17 @@ foreach ( $terms as $term )
     {
         if($count==0)
         {
-            echo ' | Type: '.$term->name;
+            echo ' | <a href="'.home_url( ).'/product-category/needs/'.strtolower($term->name).'">More ';
+            echo $term->name;
             $count++;
         }else{
-            echo ',' .$term->name;
+            echo ' & ';
+            echo $term->name;
         }
-    }
-         
+    }       
 }
-   echo ' | Caps :&nbsp;'. substr(get_the_title($product_id),-3,3);   
+   echo ' related products ';
+    echo '| Caps :&nbsp;'. substr(get_the_title($product_id),-3,3);   
 }
 ?>
 </div>
